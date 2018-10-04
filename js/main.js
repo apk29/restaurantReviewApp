4f698717c7d693895062c1ef71f6b428113782da
Perry K <apk29@hotmail.com>
2018-10-04 01:27:50 -0700

    updated map

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1IjoiYXBrMjkiLCJhIjoiY2ptcmI3Yjc4MDFneTNxcXc3cDAwcnI1eiJ9.oC1K-D1mEM5TtWQOSljUSA',
+    mapboxToken: 'pk.eyJ1IjoiYXBrMjkiLCJhIjoiY2ptdTgxenZ4MDQ4djNrczF0ZGRkYTQybSJ9.Tt4A2WTmJQu8aEcIj0lffA',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
